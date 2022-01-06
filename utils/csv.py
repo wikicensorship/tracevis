f9179645b976ee3c47c9e8b283ce91babab88461
xhdix <xhdix@yahoo.com>
2022-01-06 16:14:20 +0300

    clean the csv code more

--- utils/csv.py
+++ utils/csv.py
@@ -86,47 +86,34 @@ def parse_json(file_name: str) -> list:
     return data
 
 
-def json2csv_raw(data: list) -> str:
+def data_to_csv(data: list, sort_it: bool) -> str:
     global csv_header_all
     global csv_blank_row
     global csv_prepared_row
     csv_str = csv_header_all
     last_hop = 1
     for row in data:
-        if row["hop"] < last_hop:
-            csv_str += csv_blank_row
+        if sort_it:
+            if row["hop"] > last_hop:
+                csv_str += csv_blank_row
+        else:
+            if row["hop"] < last_hop:
+                csv_str += csv_blank_row
         csv_str += csv_prepared_row.format_map(row)
         last_hop = row["hop"]
     return csv_str
 
 
-def json2csv_clean(data: list) -> str:
-    global csv_header_all
-    global csv_blank_row
-    global csv_prepared_row
-    csv_str = csv_header_all
-    data = sorted(data, key=lambda d: d['hop'])
-    last_hop = 1
-    for row in data:
-        if row["hop"] > last_hop:
-            csv_str += csv_blank_row
-        csv_str += csv_prepared_row.format_map(row)
-        last_hop = row["hop"]
-    return csv_str
-
-
-def json2csv(file_name: str, sorted: bool = True):
+def json2csv(file_name: str, sort_it: bool = True):
     if os.path.isfile(file_name):
         new_file_name = file_name.replace(".json", ".csv")
         data = parse_json(file_name)
         prepare_csv_variables(data[0].keys())
         with open(new_file_name, "w") as csvfile:
-            csv = ""
-            if sorted:
-                csv = json2csv_clean(data)
-            else:
-                csv = json2csv_raw(data)
-            if csv != "":
+            if sort_it:
+                data = sorted(data, key=lambda d: d['hop'])
+            csv = data_to_csv(data, sort_it)
+            if csv != "": # todo (xhdix): it will never be empty. we shold do better
                 print("saving measurement in csv...")
                 csvfile.write(csv)
                 print("saved: " + new_file_name)
