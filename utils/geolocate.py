ba26390f30e2ad0b9bd170ea6f914e4e40001c1d
Ariyan Eghbal <ariyan.eghbal@gmail.com>
2022-07-16 14:24:13 +0430

    fix: fix privilege drop bypass for windows

--- utils/geolocate.py
+++ utils/geolocate.py
@@ -15,14 +15,15 @@ import pwd, grp
 OS_NAME = platform.system()
 
 def drop_privileges(uid_name='nobody', gid_name='nogroup'):
-    if os.getuid() == 0:
-        running_uid = pwd.getpwnam(uid_name).pw_uid
-        running_gid = grp.getgrnam(gid_name).gr_gid
+    if os.name == 'posix':
+        if os.getuid() == 0:
+            running_uid = pwd.getpwnam(uid_name).pw_uid
+            running_gid = grp.getgrnam(gid_name).gr_gid
 
-        os.setgroups([])
-        os.setgid(running_gid)
-        os.setuid(running_uid)
-        os.umask(0o077)
+            os.setgroups([])
+            os.setgid(running_gid)
+            os.setuid(running_uid)
+            os.umask(0o077)
 
 
 def get_meta_json():
