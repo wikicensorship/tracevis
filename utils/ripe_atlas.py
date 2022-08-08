fd1b19678f9b2da60243a1590c1ff86aa873d570
xhdix <xhdix@yahoo.com>
2022-08-08 12:21:21 +0300

    fix: use exit() correctly /2

--- utils/ripe_atlas.py
+++ utils/ripe_atlas.py
@@ -61,7 +61,7 @@ def download_from_atlas(
         print(
             " ********************************************************************** ")
         if len(all_measurements) < 1:
-            sys.exit()
+            sys.exit(1)
         measurement_path = output_dir + measurement_name + ".json"
         print("saving json file... to: " + measurement_path)
         with open((measurement_path), 'w', encoding='utf-8') as json_file:
