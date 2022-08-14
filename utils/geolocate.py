483d379bd06faef1cea401383a947d5ab5725ed1
Ariyan Eghbal <ariyan.eghbal@gmail.com>
2022-08-15 01:34:37 +0430

    [feat]: different method for windows and posix systems
    we use independent process in posix system to make privilege dropping
    stable and thread based approach in windows to prevent windows system
    get suspicious and block requests

--- utils/geolocate.py
+++ utils/geolocate.py
@@ -5,8 +5,9 @@ import json
 import time
 import platform
 from urllib.request import Request, urlopen
+from multiprocessing import Process, Value, RawArray 
+import ctypes 
 from threading import Thread 
-
 
 
 OS_NAME = platform.system()
@@ -45,7 +46,70 @@ def gain_privileges(uid, gid):
         os.seteuid(uid)
 
 
-def run_geolocate():
+def posix_run_geolocate():
+    def get_meta(no_internet, public_ip, network_asn, network_name, country_code, city):
+
+        no_internet.value = True
+        public_ip.value = '127.1.2.7'  # we should know that what we are going to clean
+        network_asn.value = 'AS0'
+        network_name.value = ''
+        country_code.value = ''
+        city.value = ''
+
+        print("· - · · · detecting IP, ASN, country, etc (Posix) · - · · · ")
+        user_meta = get_meta_json()
+        if user_meta is not None :
+            no_internet.value = False
+            if 'clientIp' in user_meta.keys():
+                public_ip.value = user_meta['clientIp']
+                print("· · · - · " + public_ip.value)
+                print('. - . - . we use public IP to know what to remove from data!')
+            if 'asn' in user_meta.keys():
+                network_asn.value = ("AS" + str(user_meta['asn']))
+                print("· · · - · " + network_asn.value)
+            if 'asOrganization' in user_meta.keys():
+                network_name.value = user_meta['asOrganization']
+                print("· · · - · " + network_name.value)
+            if 'country' in user_meta.keys():
+                country_code.value = user_meta['country']
+                print("· · · - · " + country_code.value)
+            if 'city' in user_meta.keys():
+                city.value = user_meta['city']
+                print("· · · - · " + city.value)
+
+
+    user_meta_info_timeout = 10   # Seconds
+    no_internet = True 
+    public_ip = ""
+    network_asn = ""
+    network_name = ""
+    country_code = ""
+    city = ""
+
+    user_meta_info_start_time = 0
+
+    no_internet = Value(ctypes.c_bool, True)
+    public_ip = RawArray(ctypes.c_wchar, 40)
+    network_asn = RawArray(ctypes.c_wchar, 100)
+    network_name = RawArray(ctypes.c_wchar, 100)
+    country_code = RawArray(ctypes.c_wchar, 100)
+    city = RawArray(ctypes.c_wchar, 100)
+    
+    uid, gid = drop_privileges()
+    p = Process(target=get_meta, daemon=True, args=(no_internet, public_ip, network_asn, network_name, country_code, city))
+    p.start()
+    user_meta_info_start_time = time.time()
+
+    while time.time() - user_meta_info_start_time < user_meta_info_timeout and no_internet: 
+        time.sleep(1)
+    
+    gain_privileges(uid, gid)
+
+    return no_internet.value, public_ip.value, network_asn.value, network_name.value, country_code.value, city.value
+
+
+
+def windows_run_geolocate():
     def get_meta():
         nonlocal no_internet, public_ip, network_asn, network_name, country_code, city, is_canceled
 
@@ -57,7 +121,7 @@ def run_geolocate():
         city = ''
 
 
-        print("· - · · · detecting IP, ASN, country, etc · - · · · ")
+        print("· - · · · detecting IP, ASN, country, etc (Windows) · - · · · ")
         user_meta = get_meta_json()
         if is_canceled:
             return
@@ -92,7 +156,6 @@ def run_geolocate():
 
     user_meta_info_start_time = 0
 
-    uid, gid = drop_privileges()
     p = Thread(target=get_meta, daemon=True)
     p.start()
     user_meta_info_start_time = time.time()
@@ -101,6 +164,11 @@ def run_geolocate():
     if no_internet:
         is_canceled = True
         
-    gain_privileges(uid, gid)
     return no_internet, public_ip, network_asn, network_name, country_code, city
 
+
+
+def run_geolocate():
+    if os.name == "posix":
+        return posix_run_geolocate()
+    return windows_run_geolocate()
